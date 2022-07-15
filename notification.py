b2e219d1c7b8910724babf0a9a98d83ea601bf08
Wangkang <simplepimple@163.com>
2022-07-15 14:37:15 +0800

    add action enviroment param

--- notification.py
+++ notification.py
@@ -3,20 +3,27 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
+MAIL_ENABLE = os.environ['MAIL_ENABLE']
+MAIL_HOST = os.environ['MAIL_HOST']
+MAIL_ACCOUNT = os.environ['MAIL_ACCOUNT']
+MAIL_PW = os.environ['MAIL_PW']
+MAIL_SENDER = os.environ['MAIL_SENDER']
+MAIL_RECEIVER = os.environ['MAIL_RECEIVER']
+
 # ===== cqhttp配置 =====
 cqhttp = False  # 是否启用cqhttp
 api = "http://127.0.0.1:5700/send_msg"  # cqhttp http API 地址
 uid = ""  # 收信QQ号，不填则不发送
 gid = ""  # 收信群号，不填则不发送
 # ===== 邮件设置 =====
-mail = False  # 是否启用邮件
+mail = MAIL_ENABLE  # 是否启用邮件
 ssl = True  # 是否启用SSL
-host = ""  # SMTP服务器地址（如smtp.qq.com）
+host = "MAIL_HOST"  # SMTP服务器地址（如smtp.qq.com）
 port = 465  # 输入SMTP服务器端口（如465）
-account = ""  # 发信账号
-password = ""  # 发信密码
-sender = ""  # 发信人邮箱
-receiver = ""  # 收信人邮箱
+account = "MAIL_ACCOUNT"  # 发信账号
+password = "MAIL_PW"  # 发信密码
+sender = "MAIL_SENDER"  # 发信人邮箱
+receiver = "MAIL_RECEIVER"  # 收信人邮箱
 
 
 def msg(text):
