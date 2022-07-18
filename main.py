b71bdf32656d6713709d3a82d206d5222cbc8a71
Eldoradooo <109326501+Eldoradooo@users.noreply.github.com>
2022-07-18 10:01:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,8 @@ import os
 STUID = os.environ['STUID']
 PW = os.environ['PW']
 
-# 在此填写账号密码，账号通常为学号，密码通常为身份证后6位，可添加更多用户。注意：需要解绑微信
-user_list = [
-    {"account": "STUID", "password": "PW"},
-    # {"account": "", "password": ""},
-    # {"account": "", "password": ""},
-    # {"account": "", "password": ""},
-]
-
 if __name__ == "__main__":
-    for user in user_list:
-        text = report(user["account"], user["password"])
+        text = report(STUID, PW)
         try:
             msg(text)
         except:
