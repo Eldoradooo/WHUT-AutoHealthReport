e8789ffae7ea56fe6f4bc5c4689b83838643f26f
Eldoradooo <109326501+Eldoradooo@users.noreply.github.com>
2022-07-18 10:20:43 +0800

    Update notification.py

--- notification.py
+++ notification.py
@@ -5,11 +5,10 @@ from email.utils import formataddr
 import os
 
 #MAIL_ENABLE = os.environ['MAIL_ENABLE']
-MAIL_HOST = os.environ['MAIL_HOST']
-MAIL_ACCOUNT = os.environ['MAIL_ACCOUNT']
-MAIL_PW = os.environ['MAIL_PW']
-MAIL_SENDER = os.environ['MAIL_SENDER']
-MAIL_RECEIVER = os.environ['MAIL_RECEIVER']
+host = os.environ['MAIL_HOST']
+sender = os.environ['MAIL_SENDER']
+password = os.environ['MAIL_PW']
+receiver = os.environ['MAIL_RECEIVER']
 
 # ===== cqhttp配置 =====
 cqhttp = False  # 是否启用cqhttp
@@ -19,12 +18,12 @@ gid = ""  # 收信群号，不填则不发送
 # ===== 邮件设置 =====
 mail = True  # 是否启用邮件
 ssl = True  # 是否启用SSL
-host = MAIL_HOST  # SMTP服务器地址（如smtp.qq.com）
+# host = MAIL_HOST  # SMTP服务器地址（如smtp.qq.com）
 port = 465  # 输入SMTP服务器端口（如465）
-account = MAIL_ACCOUNT  # 发信账号
-password = MAIL_PW  # 发信密码
-sender = MAIL_SENDER  # 发信人邮箱
-receiver = MAIL_RECEIVER  # 收信人邮箱
+# account = MAIL_ACCOUNT  # 发信账号
+# password = MAIL_PW  # 发信密码
+# sender = MAIL_SENDER  # 发信人邮箱
+# receiver = MAIL_RECEIVER  # 收信人邮箱
 
 
 def msg(text):
@@ -55,7 +54,7 @@ def send_mail(text):
         server = smtplib.SMTP_SSL(host, port)
     else:
         server = smtplib.SMTP(host, port)
-    server.login(account, password)
+    server.login(sender, password)
     mail_msg = MIMEText(text, "plain", "utf-8")
     mail_msg["From"] = formataddr(["WHUT-AutoHealthReport", sender])
     mail_msg["Subject"] = "【WHUT-AutoHealthReport】"
